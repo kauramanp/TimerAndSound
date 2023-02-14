0d22aeb8e6c6818dd1360252a5fadf04acc9cfa3
kauramanp <33446281+kauramanp@users.noreply.github.com>
2023-02-14 23:59:29 +0530

    Initial commit

---
+++ app/src/main/java/com/aman/timerandsound/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.aman.timerandsound
+
+import android.app.Dialog
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.aman.timerandsound.databinding.ActivityMainBinding
+import com.aman.timerandsound.databinding.LayoutDialogBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var timerTime = 0L
+    lateinit var timer: CountDownTimer
+    lateinit var mediaPlayer: MediaPlayer
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        mediaPlayer = MediaPlayer.create(this, R.raw.tick)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnTimer.setOnClickListener {
+            var layout = LayoutDialogBinding.inflate(layoutInflater)
+            var dialogs = Dialog(this)
+            dialogs.setContentView(layout.root)
+            dialogs.window?.setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            dialogs.show()
+            layout.btnSet.setOnClickListener {
+                if (layout.etTime.text.isNullOrEmpty()) {
+                    layout.etTime.error = resources.getString(R.string.enter_time)
+                    layout.etTime.requestFocus()
+                } else {
+                    timerTime = layout.etTime.text.toString().toLong()
+                    updateTimer()
+                    dialogs.dismiss()
+                }
+            }
+        }
+
+        binding.ibPlay.setOnClickListener {
+            if (this::timer.isInitialized) {
+                timer.start()
+            }
+        }
+        binding.ibStop.setOnClickListener {
+            if (this::timer.isInitialized) {
+                timer.cancel()
+            }
+        }
+
+
+    }
+
+    fun updateTimer() {
+        if (this::timer.isInitialized) {
+            timer.cancel()
+        }
+        timer = object : CountDownTimer(timerTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                Log.d("millis", millisUntilFinished.toString())
+                binding.tvTime.setText((millisUntilFinished / 1000).toString())
+                mediaPlayer.start()
+            }
+
+            override fun onFinish() {
+                mediaPlayer.stop()
+
+            }
+        }
+    }
+
+}
